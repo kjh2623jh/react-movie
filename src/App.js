830a16d21fd667e15866f9e875817d5e37f3ac46
훈제김치 <kjh2623kjh@gmail.com>
2024-05-20 16:29:46 +0900

    todo on react

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
-function Hello () {
-  useEffect(() => {
-    console.log("Created");
-    return () => console.log("Destroyed");
-  }, [])
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const [showing, setShowing] = useState(false);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
 
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
 
-  console.log("I run all the time");
-  useEffect(() =>{
-    console.log("Call the API");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("Search For", keyword);
+    if (toDo === "") {
+      return;
     }
-  }, [keyword]);
+    setToDos((curArr) => [toDo, ...curArr]);
+    setToDo("");
+  };
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={() => setShowing((prev) => !prev)}>{showing ? "Hide" : "Show"}</button>
-
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here"></input>
-      <h1 className={styles.title}>Hello</h1>
-      <button onClick={onClick}>{counter}</button>
-      <Button text={"asd"}/>
+      <h1>todos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((todo, idx) => (
+          <li key={idx}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
